fe3297cc79d5f063b1edb4659a88b618e315e533
Nguyen1976 <nguyen2202794@gmail.com>
2024-09-17 17:56:44 +0700

    Install and Config Sass

---
+++ blog/src/index.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express');//nạp thư viện
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+
+const app = express();//khởi tạo 1 cái func app
+const port = 3000;//run website ở cái cổng này
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//HTTP logger
+app.use(morgan('combined'));
+
+//Template engine
+app.engine('hbs', engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+
+//Cấu hình đường dẫn tới thư mục views
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+//định nghĩa cái route
+app.get('/', (req, res) => {
+    res.render(`home`);
+})
+app.get('/news', (req, res) => {
+    res.render(`news`);
+})
+
+//127.0.0.1 - localhost
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
