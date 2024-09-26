cf47ebd5ecbf89b0b4e1fc7bdc09d355c508de83
Nguyen1976 <nguyen2202794@gmail.com>
2024-09-26 09:48:55 +0700

    [CRUD] Create Course and Update Course

--- blog/src/app/models/Course.js
+++ blog/src/app/models/Course.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator');
+
+mongoose.plugin(slug);
 const Schema = mongoose.Schema;
 
-const Course = new Schema({
-    name: { type: String, maxLength: 255 },
-    description: { type: String, maxLength: 600 },
-    image: { type: String, maxLength: 255 }
-}, {
-    timestamps: true, //Tự động thêm createdAt updatedAt
-})
+const Course = new Schema(
+    {
+        name: { type: String, required: true },
+        description: { type: String },
+        image: { type: String },
+        videoId: { type: String, required: true },
+        level: { type: String },
+        slug: { type: String, slug: 'name', unique: true },
+    },
+    {
+        timestamps: true,
+    },
+);
 
 module.exports = mongoose.model('Course', Course);
--- blog/src/index.js
+++ blog/src/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express'); //nạp thư viện
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const { engine } = require('express-handlebars');
 
 const app = express(); //khởi tạo 1 cái func app
@@ -22,6 +23,7 @@ app.use(
 );
 app.use(express.json());
 
+app.use(methodOverride('_method'))
 //HTTP logger
 app.use(morgan('combined'));
 
@@ -30,6 +32,9 @@ app.engine(
     'hbs',
     engine({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        }
     }),
 );
 app.set('view engine', 'hbs');
